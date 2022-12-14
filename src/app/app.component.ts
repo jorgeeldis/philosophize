b72cf8c5705985e9ca87dcf54d670f33b572f42f
Jorge Gonzalez <jorgeeldisg30@gmail.com>
2022-12-14 10:59:34 -0500

    Side Menu changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Home', url: '/folder/Home', icon: 'home' },
+    { title: 'Quotes', url: '/folder/Quotes', icon: 'chatbox' },
+    { title: 'Meditations', url: '/folder/Meditations', icon: 'reader' },
+    { title: 'To-Do', url: '/folder/To-Do', icon: 'create' },
+    { title: 'Lessons', url: '/folder/Lessons', icon: 'checkbox' },
+    { title: 'About', url: '/folder/About', icon: 'information-circle' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
